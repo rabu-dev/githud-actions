b3809ffaf19ea5843bdcec089bec49b6ba213773
rabu-dev <109245617+rabu-dev@users.noreply.github.com>
2026-06-16 00:32:59 +0200

    init: python project with CI

---
+++ main.py
@@ -0,0 +1,10 @@
+def suma(a, b):
+    return a + b
+
+
+def main():
+    print(f"2 + 3 = {suma(2, 3)}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test_main.py
@@ -0,0 +1,7 @@
+from main import suma
+
+
+def test_suma():
+    assert suma(2, 3) == 5
+    assert suma(-1, 1) == 0
+    assert suma(0, 0) == 0
